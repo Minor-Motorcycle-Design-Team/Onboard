bb17ad54404e3a26d0cea149201054df4cfa0210
tillerkiller247 <80928845+tillerkiller247@users.noreply.github.com>
2021-03-20 18:28:34 -0500

    Update second.cpp

--- second.cpp
+++ second.cpp
@@ -1,72 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
+import serial
+import time
+ser = serial.Serial(/dev/serial0, 115200, timeout = 1)
 
-int main(int argc, char* argv[]) {
+#ser.write(0x42)
+ser.write(bytes(b'B'))
+#ser.write(0x57)
+ser.write(bytes(b'W'))
 
-    struct termios serial;
-    stdin std = "Hello";
-    char buffer[10];
+#ser.write(0x02)
+ser.write(bytes(2))
+#ser.write(0x00)
+ser.write(bytes(0))
+#ser.write(0x00)
+ser.write(bytes(0))
+#ser.write(0x00)
+ser.write(bytes(0))
+#ser.write(0x01)
+ser.write(bytes(1))
+#ser.write(0x06)
+ser.write(bytes(6))
 
-    if (argc == 1) {
-        printf("Usage: %s [device]\n\n", argv[0]);
-        return -1;
-    }
-
-    printf("Opening %s\n", argv[1]);
-
-    int fd = open(argv[1], O_RDWR | O_NOCTTY | O_NDELAY);
-
-    if (fd == -1) {
-        perror(argv[1]);
-        return -1;
-    }
-
-    if (tcgetattr(fd, &serial) < 0) {
-        perror("Getting configuration");
-        return -1;
-    }
-
-    // Set up Serial Configuration
-    serial.c_iflag = 0;
-    serial.c_oflag = 0;
-    serial.c_lflag = 0;
-    serial.c_cflag = 0;
-
-    serial.c_cc[VMIN] = 0;
-    serial.c_cc[VTIME] = 0;
-
-    serial.c_cflag = B115200 | CS8 | CREAD;
-
-    tcsetattr(fd, TCSANOW, &serial); // Apply configuration
-
-    // Attempt to send and receive
-    printf("Sending: %s\n", std);
-
-    int wcount = write(fd, std, strlen(str));
-    if (wcount < 0) {
-        perror("Write");
-        return -1;
-    }
-    else {
-        printf("Sent %d characters\n", wcount);
-    }
-
-    int rcount = read(fd, buffer, sizeof(buffer));
-    if (rcount < 0) {
-        perror("Read");
-        return -1;
-    }
-    else {
-        printf("Received %d characters\n", rcount);
-    }
-
-    buffer[rcount] = '\0';
-
-    printf("Received: %s\n", buffer);
-
-    close(fd);
-}
+while(True):
+    while(ser.in_waiting >= 9):
+        #print (ser.read())
+        if((b'Y' == ser.read()) and ( b'Y' == ser.read())):
+            Dist_L = ser.read()
+            Dist_H = ser.read()
+            Dist_Total = (ord(Dist_H) * 256) + (ord(Dist_L))
+            for i in range (0,5):
+                ser.read()
+                print(Dist_Total)
