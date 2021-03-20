dc78ff3be7581499c0efd42fcd5df117912b88d6
tillerkiller247 <80928845+tillerkiller247@users.noreply.github.com>
2021-03-20 18:47:34 -0500

    Update second.cpp

--- second.cpp
+++ second.cpp
@@ -1,32 +1,46 @@
-import serial
-import time
-ser = serial.Serial(/dev/serial0, 115200, timeout = 1)
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
 
-//#ser.write(0x42)
-ser.write(bytes(b'B'))
-//#ser.write(0x57)
-ser.write(bytes(b'W'))
+using namespaces std;
 
-//#ser.write(0x02)
-ser.write(bytes(2))
-//#ser.write(0x00)
-ser.write(bytes(0))
-//#ser.write(0x00)
-ser.write(bytes(0))
-//#ser.write(0x00)
-ser.write(bytes(0))
-//#ser.write(0x01)
-ser.write(bytes(1))
-//#ser.write(0x06)
-ser.write(bytes(6))
-
-while(True):
-    while(ser.in_waiting >= 9):
-        //#print (ser.read())
-        if((b'Y' == ser.read()) and ( b'Y' == ser.read())):
-            Dist_L = ser.read()
-            Dist_H = ser.read()
-            Dist_Total = (ord(Dist_H) * 256) + (ord(Dist_L))
-            for i in range (0,5):
-                ser.read()
-                print(Dist_Total)
+int main(int argc, char** argv)
+{
+	int uart0_filestream = -1;
+	uart0_filestream = open("/dev/ttyUSB1", O_RDONLY | O_NOCTTY | O_NDELAY);
+	if (uart0_filestream == -1)
+	{
+		printf("Error - Unable to open UART")
+	}
+	struct termios options;
+	tcgetattr(uart0_filestream, &options);
+	options.c_cflag = B9600 | CS8 | PARENB | CLOCAL | CREAD;
+	options.c_iflag = IGNPAR;
+	options.c_oflag = 0;
+	options.c_lflag = 0;
+	tcflush(uart0_filestream, TCIFLUSH);
+	tcsetattr(uart0_filestream, TCSANOW, &options);
+	if (uart0_filestream != -1)
+	{
+		unsigned char rx_buffer[256];
+		int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);
+		while (rx_length < 255)
+		{
+			if (rx_length < 0)
+			{
+				cout<<"An error occured"<<endl;
+			}
+			else if (rx_length == 0)
+			{
+				cout<<"No data, waiting"<<endl;
+			}
+			else
+			{
+				rx_buffer[rx_length] = '\0';
+				printf("%i bytes read: %s\n", rx_length, rx_buffer);
+			}
+		}
+	}
+	close(uart0_filestream);
+}
