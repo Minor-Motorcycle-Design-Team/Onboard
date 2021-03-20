2ac23b856be9b561a8a5f4a4cc345c98ae4adcea
tillerkiller247 <80928845+tillerkiller247@users.noreply.github.com>
2021-03-20 18:10:05 -0500

    Update second.cpp

--- second.cpp
+++ second.cpp
@@ -1,28 +1,25 @@
-#include "rs232.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <iostream>
+#include <wiringPi.h>
+#include <wiringSerial.h>
 
-#define COMPORT			16 		// this is '/dev/ttyUSB0' (for arduino) or choose wherever you added '/dev/ttyAMA0' (for raspberry pi UART pins) to the list
-#define BAUDRATE		9600	// or whatever baudrate you want
-#define RECEIVE_CHARS	8		// or whatever size of buffer you want to receive
-#define SEND_CHARS		4		// or whatever size of buffer you want to send
-
-int main (int argc, char **argv) {
-	unsigned char receive_buffer[RECEIVE_CHARS];
-	unsigned char send_byte = 42;
-	unsigned char send_buffer[SEND_CHARS] = {'a','b','c','d'};
-
-	OpenComport(COMPORT, BAUDRATE);
-	while(1) {
-		// change send_byte and/or send_buffer with what you want to send. Then:
-		SendByte(COMPORT, send_byte); // or:
-		SendBuf(COMPORT, send_buffer, SEND_CHARS);
-		
-		// and/or:
-		PollComport(COMPORT, receive_buffer, RECEIVE_CHARS);
-
-		// do something with received data in buffer
-		// maybe sleep for a while
+int
+open_port(void)
+{
+	int fd;
+	fd = open("/dev/ttyfl" , O_RDWR | O_NOCTTY | O_NDELAY);
+	if (fd == -1)
+	{
+		perror ("open_port: Unable to open /dev/ttyfl -");
 	}
-	CloseComport(COMPORT);
-	
-	return 0;
+	else
+	{
+		fcntl(fd, F_SETFL, 0);
+	}
+	return (fd);
 }
