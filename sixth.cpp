195a2694eafb787e73e27de65eb32af4919ce28a
tillerkiller247 <80928845+tillerkiller247@users.noreply.github.com>
2021-03-21 22:43:48 -0500

    Create sixth.cpp

---
+++ sixth.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+  int fd;
+  if ((fd = serialOpen ("/dev/serial0", 9600)) < 0)
+  {
+    cout<< "unable to open device";
+    return 1;
+  }
+  else
+  {
+    cout<< "ttyAMA0 initialised ok/n";
+  }
+  char cmd[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
+  char cmd2[9];
+  cout<<"Tilt Angle Sensor is Running";
+  While (1)
+  {
+    delay(1000);
+    write (fd,cmd,9);
+    read (fd, cmd2, 9);
+    for (int loop = 0; loop <= 8; loop++)
+    {
+      cout<< (int)cmd2[loop] << "\t";
+      cout<< "\n";
+      cout<< "\n" << cmd2;
+    }
+  }
+}
